b24bdfe6d9073222a5fbd0637de24761707f885e
Hanako Amaya <floppies.amaya@gmail.com>
2026-01-16 20:55:00 -0300

    Cambiando StallID por IFIDWrite y arreglando la logica para tener un habilitador del registro para escritura

--- sources/IF_ID_reg.v
+++ sources/IF_ID_reg.v
@@ -8,7 +8,7 @@ module IF_ID_reg    #(
     input   wire    i_clk   ,   i_rst,  flush   ,
     input   wire    [MSB-1:0]   IF_next_pc      ,
     input   wire    [MSB-1:0]   IF_inst         ,
-    input   wire                stall_ID        ,
+    input   wire                We              ,
     //Salidas
     output  reg     [MSB-1:0]   ID_next_pc      ,
     output  reg     [MSB-1:0]   ID_inst
@@ -16,15 +16,18 @@ module IF_ID_reg    #(
 
 always  @(posedge i_clk)
     begin
-        if  (i_rst  ||  flush)
+        if(We)
         begin
-            ID_next_pc  <=      32'b0       ;
-            ID_inst     <=      32'b0       ;
-        end
-        else if (!stall_ID)
-        begin
-            ID_next_pc  <=      IF_next_pc  ;
-            ID_inst     <=      IF_inst     ;
+            if  (i_rst  ||  flush)
+            begin
+                ID_next_pc  <=      32'b0       ;
+                ID_inst     <=      32'b0       ;
+            end
+            else
+            begin
+                ID_next_pc  <=      IF_next_pc  ;
+                ID_inst     <=      IF_inst     ;
+            end
         end
     end
 
--- sources/simulacion/if_id_reg_tb.v
+++ sources/simulacion/if_id_reg_tb.v
@@ -8,22 +8,28 @@ module if_id_reg_tb();
     //Entradas
     reg     [MSB-1:0]   if_nxt_pc   ,   if_inst     ;
     reg     i_clk   ,   i_rst       ,   flush       ;
+    reg     i_if_id_write                           ;
     //Salidas
     wire    [MSB-1:0]   id_nxt_pc   ,   id_inst     ;
     
     initial begin
         $dumpfile("dump.vcd"); $dumpvars;
         i_clk       =       1       ;
-        i_rst       =       1       ;
+        i_rst       =       1       ;   // Reset
+        i_if_id_write =     1       ;
         flush       =       0       ;
         if_nxt_pc   =       4       ;
         if_inst     =       5       ;
         #5
         i_rst       =       0       ;
         #10
-        if_nxt_pc   =       8       ;
+        if_nxt_pc   =       8       ;   // Save instruction
         if_inst     =       6       ;
+        #5
+        i_if_id_write =     0       ;   // Stall
         flush       =       1       ;
+        #10
+        i_if_id_write =     1       ;   // Enable and flush
         #10
         flush       =       0       ;
         #20
@@ -43,6 +49,7 @@ module if_id_reg_tb();
         .i_clk          (i_clk)     ,
         .i_rst          (i_rst)     ,
         .flush          (flush)     ,
+        .We             (i_if_id_write) ,
         .IF_next_pc     (if_nxt_pc) ,
         .IF_inst        (if_inst)   ,
         .ID_next_pc     (id_nxt_pc) ,
