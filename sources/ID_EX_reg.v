3e0ce18a9cfcf28088807ca0a1ebf44bbf702e67
Hanako Amaya <floppies.amaya@gmail.com>
2026-01-19 16:52:27 -0300

    Cambiando el reg de ID/EX para tener un We y un flush

--- sources/ID_EX_reg.v
+++ sources/ID_EX_reg.v
@@ -3,73 +3,66 @@
 module ID_EX_reg    #(
     parameter       NBITS   =   32      ,
     parameter       RBITS   =   5       ,
-    parameter       FBITS   =   6       
+    parameter       FBITS   =   10       
 )
 (
     //Entradas
-    input   wire    i_clk   ,       i_rst,  i_nop   ,
-    input   wire    [NBITS-1:0]     ID_Rs,  ID_Rt   ,   //Datos de los registros
-    input   wire    [RBITS-1:0]     ID_rd,  ID_rt   ,   //Nombre de los registros
-    input   wire    [FBITS-1:0]     ID_funct        ,
-    input   wire    [NBITS-1:0]     ID_immediate    ,
-    input   wire    [4:0]           ID_sizecontrol  ,
-    input   wire    ID_memtoreg,    ID_memread      ,
-                    ID_memwrite,    ID_alusource    ,
-                    ID_link,        ID_regwrite     ,
-                                    ID_haltflag     ,
-    input   wire    [2:0]           ID_aluop        ,
-    input   wire    [1:0]           ID_regdst       ,
+    input   wire    i_clk   ,       i_rst,  ID_EX_flush ,
+    input   wire    We                                  ,
+    input   wire    [NBITS-1:0]     ID_Rs1, ID_Rs2      ,   //Register data
+    input   wire    [NBITS-1:0]     next_pc             ,   //PC+4
+    input   wire    [RBITS-1:0]     ID_rd               ,   //Register address
+    input   wire    [FBITS-1:0]     ID_funct            ,   //funct7,funct3
+    input   wire    [NBITS-1:0]     ID_immediate        ,
+    input   wire    ID_memtoreg,    ID_memread          ,
+                    ID_memwrite,    ID_alusource        ,
+                    ID_link,        ID_regwrite         ,
+    input   wire    [2:0]           ID_aluop            ,
     //Salidas
-    output  reg     [NBITS-1:0]     EX_Rs,  EX_Rt   ,   //Datos de los registros
-    output  reg     [RBITS-1:0]     EX_rd,  EX_rt   ,   //Nombre de los registros
-    output  reg     [FBITS-1:0]     EX_funct        ,
-    output  reg     [NBITS-1:0]     EX_immediate    ,
-    output  reg     [4:0]           EX_sizecontrol  ,
-    output  reg     EX_memtoreg,    EX_memread      ,
-                    EX_memwrite,    EX_alusource    ,
-                    EX_link,        EX_regwrite     ,
-                                    EX_haltflag     ,
-    output  reg     [2:0]           EX_aluop        ,
-    output  reg     [1:0]           EX_regdst
+    output  reg     [NBITS-1:0]     EX_Rs1, EX_Rs2      ,   //Registers data
+    output  reg     [RBITS-1:0]     EX_rd               ,   //Register address
+    output  reg     [FBITS-1:0]     EX_funct            ,   //funct7,funct3
+    output  reg     [NBITS-1:0]     EX_immediate        ,
+    output  reg     EX_memtoreg,    EX_memread          ,
+                    EX_memwrite,    EX_alusource        ,
+                    EX_link,        EX_regwrite         ,
+    output  reg     [2:0]           EX_aluop
 );
 
 always  @(posedge i_clk)
     begin
-        if  (i_rst  ||  i_nop)
+        if (We)
         begin
-            EX_Rs           <=      32'b0           ;
-            EX_Rt           <=      32'b0           ;
-            EX_immediate    <=      32'b0           ;
-            EX_rd           <=      5'b0            ;
-            EX_rt           <=      5'b0            ;
-            EX_funct        <=      6'b0            ;
-            EX_sizecontrol  <=      5'b0            ;
-            EX_regwrite     <=      0               ;
-            EX_memtoreg     <=      0               ;
-            EX_memread      <=      0               ;
-            EX_memwrite     <=      0               ;
-            EX_alusource    <=      0               ;
-            EX_link         <=      0               ;
-            EX_aluop        <=      2'b0            ;
-            EX_regdst       <=      2'b0            ;
-        end
-        else
-        begin
-            EX_Rs           <=      ID_Rs           ;
-            EX_Rt           <=      ID_Rt           ;
-            EX_immediate    <=      ID_immediate    ;
-            EX_rd           <=      ID_rd           ;
-            EX_rt           <=      ID_rt           ;
-            EX_funct        <=      ID_funct        ;
-            EX_sizecontrol  <=      ID_sizecontrol  ;
-            EX_regwrite     <=      ID_regwrite     ;
-            EX_memtoreg     <=      ID_memtoreg     ;
-            EX_memread      <=      ID_memread      ;
-            EX_memwrite     <=      ID_memwrite     ;
-            EX_alusource    <=      ID_alusource    ;
-            EX_link         <=      ID_link         ;
-            EX_aluop        <=      ID_aluop        ;
-            EX_regdst       <=      ID_regdst       ;
+            if  (i_rst  ||  ID_EX_flush)
+            begin
+                EX_Rs1          <=      32'b0           ;
+                EX_Rs2          <=      32'b0           ;
+                EX_immediate    <=      32'b0           ;
+                EX_rd           <=      5'b0            ;
+                EX_funct        <=      10'b0           ;
+                EX_regwrite     <=      0               ;
+                EX_memtoreg     <=      0               ;
+                EX_memread      <=      0               ;
+                EX_memwrite     <=      0               ;
+                EX_alusource    <=      0               ;
+                EX_link         <=      0               ;
+                EX_aluop        <=      2'b0            ;
+            end
+            else
+            begin
+                EX_Rs1          <=      ID_Rs1          ;
+                EX_Rs2          <=      ID_Rs2          ;
+                EX_immediate    <=      ID_immediate    ;
+                EX_rd           <=      ID_rd           ;
+                EX_funct        <=      ID_funct        ;
+                EX_regwrite     <=      ID_regwrite     ;
+                EX_memtoreg     <=      ID_memtoreg     ;
+                EX_memread      <=      ID_memread      ;
+                EX_memwrite     <=      ID_memwrite     ;
+                EX_alusource    <=      ID_alusource    ;
+                EX_link         <=      ID_link         ;
+                EX_aluop        <=      ID_aluop        ;
+            end
         end
     end
 
--- sources/simulacion/id_ex_reg_tb.v
+++ sources/simulacion/id_ex_reg_tb.v
@@ -3,93 +3,108 @@
 module id_ex_reg_tb();
 
     //Parametros
-    localparam      NBITS       =       32      ;
-    localparam      RBITS       =       5       ;
-    localparam      FBITS       =       6       ;
-    
+    localparam      NBITS       =   32      ;
+    localparam      RBITS       =   5       ;
+    localparam      FBITS       =   10      ;
+
     //Entradas
-    reg     [NBITS-1:0]     id_Rs   ,   id_Rt   ;
-    reg     [NBITS-1:0]             id_imm      ;
-    reg     [RBITS-1:0]     id_rd   ,   id_rt   ;
-    reg     [FBITS-1:0]             id_funct    ;
-    reg             id_memtoreg ,   id_memread  ,
-                    id_memwrite ,   id_alusource,
-                    id_link     ,   id_regwrite ,
-                    i_clk       ,   i_rst       ,
-                                    stallid     ;
-    reg     [2:0]                   id_aluop    ;
-    reg     [1:0]                   id_regdst   ;
-    reg     [4:0]                   id_sizectrl ;
-    
+    reg     [NBITS-1:0]     id_rs1      ,   id_rs2      ;
+    reg     [NBITS-1:0]     id_imm      ,   id_next_pc  ;
+    reg     [RBITS-1:0]     id_rd       ;
+    reg     [FBITS-1:0]     id_funct    ;
+    reg                     id_memtoreg ,   id_memread  ,
+                            id_memwrite ,   id_alusource,
+                            id_link     ,   id_regwrite ,
+                            i_clk       ,   i_rst       ,
+                            id_ex_flush ,   we          ;
+    reg     [2:0]           id_aluop    ;
+
     //Salidas
-    wire    [NBITS-1:0]     ex_Rs   ,   ex_Rt   ;
-    wire    [NBITS-1:0]             ex_imm      ;
-    wire    [RBITS-1:0]     ex_rd   ,   ex_rt   ;
-    wire    [FBITS-1:0]             ex_funct    ;
-    wire            ex_memtoreg ,   ex_memread  ,
-                    ex_memwrite ,   ex_alusource,
-                    ex_link     ,   ex_regwrite ;
-    wire    [2:0]                   ex_aluop    ;
-    wire    [1:0]                   ex_regdst   ;
-    wire    [4:0]                   ex_sizectrl ;
+    wire    [NBITS-1:0]     ex_rs1      ,   ex_rs2      ;
+    wire    [NBITS-1:0]     ex_imm      ;
+    wire    [RBITS-1:0]     ex_rd       ;
+    wire    [FBITS-1:0]     ex_funct    ;
+    wire                    ex_memtoreg ,   ex_memread  ,
+                            ex_memwrite ,   ex_alusource,
+                            ex_link     ,   ex_regwrite ;
+    wire    [2:0]           ex_aluop    ;
 
     initial begin
         $dumpfile("dump.vcd"); $dumpvars;
-        i_clk       =       1       ;
-        i_rst       =       1       ;
-        stallid     =       0       ;
-        //Ponemos valores a los registros y sus datos
-        id_Rs       =       8       ;
-        id_Rt       =       9       ;
-        id_rd       =       2       ;
-        id_rt       =       3       ;
-        //Valores para la funcion y el inmediato
-        id_funct    =       6'h6    ;
-        id_imm      =       15      ;
-        //Valores para el control
-        id_memtoreg     =   1       ;
-        id_memread      =   1       ;
-        id_memwrite     =   1       ;
-        id_alusource    =   1       ;
-        id_link         =   1       ;
-        id_regwrite     =   1       ;
-        id_aluop        =   4       ;
-        id_regdst       =   2       ;
-        id_sizectrl     =   5       ;
+        i_clk       =   1       ;
+        i_rst       =   1       ;
+        id_ex_flush =   0       ;
+        we          =   1       ;
+
+        // Valores iniciales
+        id_rs1      =   32'h0000_0008 ;
+        id_rs2      =   32'h0000_0009 ;
+        id_rd       =   5'd2          ;
+        id_funct    =   10'h006       ;
+        id_imm      =   32'h0000_000F ;
+        id_next_pc  =   32'h0000_0004 ;
+        id_memtoreg =   1'b1          ;
+        id_memread  =   1'b1          ;
+        id_memwrite =   1'b0          ;
+        id_alusource=   1'b1          ;
+        id_link     =   1'b0          ;
+        id_regwrite =   1'b1          ;
+        id_aluop    =   3'b100        ;
+
         #5
-        i_rst           =   0       ;
+        i_rst       =   0       ;
+
+        // Carga normal
         #10
-        stallid         =   1       ;
+        id_rs1      =   32'h0000_0011 ;
+        id_rs2      =   32'h0000_0022 ;
+        id_rd       =   5'd3          ;
+        id_funct    =   10'h015       ;
+        id_imm      =   32'h0000_0008 ;
+        id_next_pc  =   32'h0000_0008 ;
+        id_memtoreg =   1'b0          ;
+        id_memread  =   1'b0          ;
+        id_memwrite =   1'b1          ;
+        id_alusource=   1'b0          ;
+        id_link     =   1'b1          ;
+        id_regwrite =   1'b0          ;
+        id_aluop    =   3'b010        ;
+
+        // Flush (debe limpiar)
         #10
-        stallid         =   0       ;
+        id_ex_flush =   1       ;
+
+        // Hold con We=0 (no cambia salidas)
         #10
-        stallid         =   1       ;
-        id_Rs       =       9       ;
-        id_Rt       =       8       ;
-        id_rd       =       3       ;
-        id_rt       =       2       ;
-        id_funct    =       6'h5    ;
-        id_imm      =       8       ;
-        id_memtoreg     =   0       ;
-        id_memread      =   0       ;
-        id_memwrite     =   1       ;
-        id_alusource    =   1       ;
-        id_link         =   1       ;
-        id_regwrite     =   0       ;
-        id_aluop        =   5       ;
-        id_regdst       =   1       ;
-        id_sizectrl     =   4       ;
+        id_ex_flush =   0       ;
+        we          =   0       ;
+        id_rs1      =   32'h0000_00AA ;
+        id_rs2      =   32'h0000_00BB ;
+        id_rd       =   5'd4          ;
+        id_funct    =   10'h3FF       ;
+        id_imm      =   32'h0000_00CC ;
+        id_next_pc  =   32'h0000_000C ;
+        id_memtoreg =   1'b1          ;
+        id_memread  =   1'b1          ;
+        id_memwrite =   1'b1          ;
+        id_alusource=   1'b1          ;
+        id_link     =   1'b1          ;
+        id_regwrite =   1'b1          ;
+        id_aluop    =   3'b111        ;
+
+        // Re-enable write (debe cargar nuevos valores)
         #10
-        stallid         =   0       ;
+        we          =   1       ;
+
         #20
         $finish;
     end
-    
+
     always begin
         #5
-        i_clk       =       ~i_clk  ;
+        i_clk       =   ~i_clk  ;
     end
-    
+
     ID_EX_reg
     #(
         .NBITS          (NBITS)     ,
@@ -99,37 +114,33 @@ module id_ex_reg_tb();
     (
         .i_clk          (i_clk)         ,
         .i_rst          (i_rst)         ,
-        .stallID        (stallid)       ,
-        .ID_Rs          (id_Rs)         ,
-        .ID_Rt          (id_Rt)         ,
+        .ID_EX_flush    (id_ex_flush)   ,
+        .We             (we)            ,
+        .ID_Rs1         (id_rs1)        ,
+        .ID_Rs2         (id_rs2)        ,
+        .next_pc        (id_next_pc)    ,
         .ID_rd          (id_rd)         ,
-        .ID_rt          (id_rt)         ,
         .ID_funct       (id_funct)      ,
         .ID_immediate   (id_imm)        ,
         .ID_memtoreg    (id_memtoreg)   ,
-        .ID_memwrite    (id_memwrite)   ,
         .ID_memread     (id_memread)    ,
+        .ID_memwrite    (id_memwrite)   ,
         .ID_alusource   (id_alusource)  ,
         .ID_link        (id_link)       ,
         .ID_regwrite    (id_regwrite)   ,
         .ID_aluop       (id_aluop)      ,
-        .ID_regdst      (id_regdst)     ,
-        .ID_sizecontrol (id_sizectrl)   ,
-        .EX_Rs          (ex_Rs)         ,
-        .EX_Rt          (ex_Rt)         ,
+        .EX_Rs1         (ex_rs1)        ,
+        .EX_Rs2         (ex_rs2)        ,
         .EX_rd          (ex_rd)         ,
-        .EX_rt          (ex_rt)         ,
         .EX_funct       (ex_funct)      ,
         .EX_immediate   (ex_imm)        ,
         .EX_memtoreg    (ex_memtoreg)   ,
-        .EX_memwrite    (ex_memwrite)   ,
         .EX_memread     (ex_memread)    ,
+        .EX_memwrite    (ex_memwrite)   ,
         .EX_alusource   (ex_alusource)  ,
         .EX_link        (ex_link)       ,
         .EX_regwrite    (ex_regwrite)   ,
-        .EX_aluop       (ex_aluop)      ,
-        .EX_regdst      (ex_regdst)     ,
-        .EX_sizecontrol (ex_sizectrl)
+        .EX_aluop       (ex_aluop)
     );
 
 endmodule
